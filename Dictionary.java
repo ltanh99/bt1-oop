c179e3df37ae424fdaae1254f2f7178d6fdfd5fb
ltanh99 <43319098+ltanh99@users.noreply.github.com>
2018-10-07 09:41:12 +0700

    Add files via upload

---
+++ Dictionary.java
@@ -0,0 +1,19 @@
+package dictionary1;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Dictionary {
+    static ArrayList<Word> arr = new ArrayList();
+    public static ArrayList<Word> getArray() {
+        return arr;
+    }
+}
